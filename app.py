d511bde78d8d2ab5f063572d2818c661364c0dcb
Kenta YAMAMOTO <lq33xxx@gmail.com>
2015-02-01 17:21:13 +0900

    Added dummy data

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import logging
 
 import tornado.ioloop
 import tornado.web
 import tornado.escape
 import tornado.options
-from tornado.options import(define,options)
+from tornado.options import(define, options)
+# from tornado_mysql import pools
 
 # Sqlalchemy imports
 from sqlalchemy import create_engine
@@ -19,21 +19,20 @@ import models
 define("port", default=5000, type=int)
 define("debug", default=False, type=bool)
 define("sqla_uri", default="")
+define("setup", default=False, type=bool)
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r'/', MainHandler),
-            # (r"/users", UsersHandler),
+            (r"/teachers", TeachersHandler),
         ]
         settings = dict(
             autoescape="xhtml_escape",
             debug=options.debug,
             )
         tornado.web.Application.__init__(self, handlers, **settings)
-        print options.sqla_uri
-        engine = create_engine(
-            options.sqla_uri, convert_unicode=True, echo=options.debug)
+        engine = create_engine(options.sqla_uri, convert_unicode=True, echo=options.debug)
         models.init_db(engine)
         self.db = scoped_session(sessionmaker(bind=engine))
 
@@ -43,17 +42,27 @@ class BaseHandler(tornado.web.RequestHandler):
     def db(self):
         return self.application.db
 
+
 class MainHandler(BaseHandler):
     def get(self):
         self.write("Hello, world")
+
+
+class TeachersHandler(BaseHandler):
+    def get(self):
+        self.write("Hello, world")
+
 
 def main():
     tornado.options.parse_config_file('app.conf')
     tornado.options.parse_command_line()
     app = Application()
+    if options.setup:
+        models.insert_dummy_data(app.db)
     app.listen(options.port)
     logging.debug('run on port %d in %s mode' % (options.port, options.logging))
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
     main()
+
--- models.py
+++ models.py
@@ -1,30 +1,64 @@
-
+# -*- coding: utf-8 -*-
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, DATETIME, Index
 from sqlalchemy.schema import Column, ForeignKey, Table, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.dialects.mysql import INTEGER as Integer
+# from sqlalchemy.dialects.mysql import INTEGER as Integer
+from sqlalchemy.types import TypeDecorator
+from sqlalchemy.orm import relation, backref
 
+import factory
+from factory.alchemy import SQLAlchemyModelFactory
+from faker import Factory
+
+import random
 from datetime import datetime
+from pprint import pprint
+
+import enum_types
 
 '''
+
 @todo
  1. Pagination
- 2. Enum for lang_id, lang_level, lesson_type
 '''
 
 Base = declarative_base()
 
 
+class EnumType(TypeDecorator):
+    """Store IntEnum as Integer"""
+
+    impl = Integer
+
+    def __init__(self, *args, **kwargs):
+        self.enum_class = kwargs.pop('enum_class')
+        TypeDecorator.__init__(self, *args, **kwargs)
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            if not isinstance(value, self.enum_class):
+                raise TypeError("Value should %s type" % self.enum_class)
+            return value.value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            if not isinstance(value, int):
+                raise TypeError("value should have int type")
+            return self.enum_class(value)
+
+
 class User(Base):
     __tablename__ = 'user'
     __table_args__ = {'mysql_engine': 'InnoDB'}
-    id = Column(Integer(unsigned=True), primary_key=True, autoincrement=True)
-    username = Column(String(30))
-    name = Column(String(30), nullable=False)
-    email = Column(String(75), nullable=False)
-    password = Column(String(128), nullable=False)
-    created = Column(DATETIME, default=datetime.now, nullable=False)
-    modified = Column(DATETIME, default=datetime.now, nullable=False)
+    id = Column('id', Integer, primary_key=True)
+    username = Column('username', String(30))
+    name = Column('name', String(30), nullable=False)
+    created = Column('created', DATETIME, default=datetime.now, nullable=False)
+    modified = Column('modified', DATETIME, default=datetime.now, nullable=False)
+    # lang_level = relationship("LangLevel")
+    # teaching_lang = relationship("TeachingLang")
+    # learning_lang = relationship("LearningLang")
+    # course = relationship("Course")
 
     def __init__(self, username, name):
         self.username = username
@@ -34,64 +68,73 @@ class User(Base):
         self.modified = now
 
     def __repr__(self):
-        return "<User('%s')>" % (self.name)
+        return "<User('%s')>" % self.id
 
 
 class LangLevel(Base):
     __tablename__ = 'lang_level'
     __table_args__ = {'mysql_engine': 'InnoDB'}
-    id = Column(Integer(unsigned=True), primary_key=True, autoincrement=True)
-    user_id = Column(Integer(unsigned=True), ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE'))
-    lang_id = Column(Integer(unsigned=True), index=True, nullable=False)
-    lang_level = Column(Integer)
+    id = Column('id', Integer, primary_key=True)
+    user_id = Column('user_id', Integer,
+                     ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    lang_code = Column('lang_code', EnumType(enum_class=enum_types.LangCode), index=True, nullable=False)
+    lang_level = Column('lang_level', EnumType(enum_class=enum_types.LangLevel))
+    user = relation("User", backref=backref('lang_level', order_by=id))
 
     def __repr__(self):
-        return "<LangLevel('user_id:%s,lang_id:%s,lang_level:%s')>" % self.user_id, self.lang_id, self.lang_level
+        return "<LangLevel('user_id:%s,lang_code:%s,lang_level:%s')>" % self.user_id, self.lang_code, self.lang_level
 
 
 class TeachingLang(Base):
     __tablename__ = 'teaching_lang'
     __table_args__ = (
-        (UniqueConstraint('user_id', 'lang_id', name='unique__idx__user_id__lang_id')),
+        (UniqueConstraint('user_id', 'lang_code', name='unique__idx__user_id__lang_code')),
         {'mysql_engine': 'InnoDB'}
     )
-    id = Column(Integer(unsigned=True), primary_key=True, autoincrement=True)
-    user_id = Column(Integer(unsigned=True), ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE'))
-    lang_id = Column(Integer(unsigned=True), index=True, nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column('user_id', Integer,
+                     ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    lang_code = Column(EnumType(enum_class=enum_types.LangCode), index=True, nullable=False)
+    user = relation("User", backref=backref('teaching_lang', order_by=id))
 
     def __repr__(self):
-        return "<TeachingLang('%s,%s')>" % self.user_id, self.lang_id
+        return "<TeachingLang('%s,%s')>" % self.user_id, self.lang_code
 
 
 class LearningLang(Base):
     __tablename__ = 'learning_lang'
     __table_args__ = (
-        (UniqueConstraint('user_id', 'lang_id', name='unique__idx__user_id__lang_id')),
+        (UniqueConstraint('user_id', 'lang_code', name='unique__idx__user_id__lang_code')),
         {'mysql_engine': 'InnoDB'}
     )
-    id = Column(Integer(unsigned=True), primary_key=True, autoincrement=True)
-    user_id = Column(Integer(unsigned=True), ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE'))
-    lang_id = Column(Integer(unsigned=True), index=True, nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column('user_id', Integer,
+                     ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    lang_code = Column(EnumType(enum_class=enum_types.LangCode), index=True, nullable=False)
+    user = relation("User", backref=backref('learning_lang', order_by=id))
 
     def __repr__(self):
-        return "<LearningLang('%s,%s')>" % self.user_id, self.lang_id
+        return "<LearningLang('%s,%s')>" % self.user_id, self.lang_code
+
 
 class Course(Base):
     __tablename__ = 'course'
     __table_args__ = (
         {'mysql_engine': 'InnoDB'}
     )
-    id = Column(Integer(unsigned=True), primary_key=True, autoincrement=True)
-    user_id = Column(Integer(unsigned=True), ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE'))
-    lang_id = Column(Integer(unsigned=True), index=True, nullable=False)
-    lesson_type = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    user_id = Column('user_id', Integer,
+                     ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    lang_code = Column(EnumType(enum_class=enum_types.LangCode), index=True, nullable=False)
+    lesson_type = Column(EnumType(enum_class=enum_types.LessonType))
     minutes = Column(Integer)
     itc = Column(Integer)
-    session_count = Column(Integer(unsigned=True), default=0)
-    rating = Column(Integer(unsigned=True), default=0)
+    session_count = Column(Integer, default=0)
+    rating = Column(Integer, default=0)
+    user = relation("User", backref=backref('course', order_by=id))
 
     def __repr__(self):
-        return "<Course('%s,%s')>" % self.user_id, self.lang_id
+        return "<Course('%s,%s')>" % self.user_id, self.lang_code
 
 
 user_table = User.__table__
@@ -104,3 +147,88 @@ metadata = Base.metadata
 
 def init_db(engine):
     Base.metadata.create_all(bind=engine)
+
+faker = Factory.create()
+
+
+def insert_dummy_data(session):
+    for i in range(10):
+        add_one_user(session)
+
+
+def add_one_user(session):
+    user = User(name=faker.name(), username=faker.user_name())
+    session.add(user)
+    session.commit()
+
+    session.add(LangLevel(user_id=user.id,
+                          lang_code=random.choice(list(enum_types.LangCode)),
+                          lang_level=random.choice(list(enum_types.LangLevel))))
+
+    session.add(TeachingLang(user_id=user.id,
+                             lang_code=random.choice(list(enum_types.LangCode))))
+
+    session.add(LearningLang(user_id=user.id,
+                             lang_code=random.choice(list(enum_types.LangCode))))
+
+    session.add(Course(user_id=user.id,
+                       lang_code=random.choice(list(enum_types.LangCode)),
+                       lesson_type=random.choice(list(enum_types.LessonType)),
+                       minutes=random.randint(1, 12) * 10,
+                       itc=random.randint(1, 100) * 10,
+                       session_count=random.randint(0, 1000),
+                       rating=random.randint(0, 5)))
+    session.commit()
+
+
+# class UserFactory(SQLAlchemyModelFactory):
+#     class Meta:
+#         model = User
+#     # id = factory.Sequence(lambda n: n)
+#     name = faker.name()
+#     username = faker.user_name()
+#     # email = faker.safe_email()
+#     # password = faker.md5()
+#     # lang_level = factory.SubFactory(LangLevelFactory)
+#     # teaching_lang = factory.SubFactory(TeachingLang)
+#     # learning_lang = factory.SubFactory(LearningLang)
+#     # course = factory.SubFactory(Course)
+#
+#
+# class LangLevelFactory(SQLAlchemyModelFactory):
+#     class Meta:
+#         model = LangLevel
+#     # id = factory.Sequence(lambda n: n)
+#     user_id = factory.SubFactory(UserFactory)
+#     lang_code = random.choice(list(enum_types.LangCode))
+#     lang_level = random.choice(list(enum_types.LangLevel))
+#
+#
+# class TeachingLangFactory(SQLAlchemyModelFactory):
+#     class Meta:
+#         model = TeachingLang
+#     # id = factory.Sequence(lambda n: n)
+#     user_id = factory.SubFactory(UserFactory)
+#     lang_code = random.choice(list(enum_types.LangCode))
+#
+#
+# class LearningLangFactory(SQLAlchemyModelFactory):
+#     class Meta:
+#         model = LearningLang
+#     # id = factory.Sequence(lambda n: n)
+#     user_id = factory.SubFactory(UserFactory)
+#     lang_code = random.choice(list(enum_types.LangCode))
+#
+#
+# class CourseFactory(SQLAlchemyModelFactory):
+#     class Meta:
+#         model = Course
+#     # id = factory.Sequence(lambda n: n)
+#     user_id = factory.SubFactory(UserFactory)
+#     lang_code = random.choice(list(enum_types.LangCode))
+#     lesson_type = random.choice(list(enum_types.LessonType))
+#     minutes = random.randint(1, 12) * 10
+#     itc = random.randint(1, 100) * 100
+#     session_count = random.randint(0, 1000)
+#     rating = random.randint(0, 5)
+
